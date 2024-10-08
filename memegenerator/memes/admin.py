2674359d0c260bf9c6655987677634afd326e4cb
Anthony <anthonyassayah14@gmail.com>
2024-10-08 14:42:52 +0300

    create superuser for Django admin

--- memegenerator/memes/admin.py
+++ memegenerator/memes/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import MemeTemplate, Meme, Rating
 
-# Register your models here.
+admin.site.register(MemeTemplate)
+admin.site.register(Meme)
+admin.site.register(Rating)
